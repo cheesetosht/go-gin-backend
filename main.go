9ba5be765a98a0d63713adb96676c672db653053
Vraj <thevrajshah@gmail.com>
2022-04-08 22:38:55 +0530

    init: created ping-pong api

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main()  {
+	r := gin.Default()
+	r.GET("/ping",func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+	fmt.Print("Hello World")
+}
